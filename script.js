6dea218f723299bacc5ed6d60b8d5b26fed59fc2
Collin Kier <collinkier@live.com>
2020-02-09 09:22:04 -0800

    added A Lot of javascript and got the temperature, humidity and city name to update. Still need to do the three day weather forecast

--- script.js
+++ script.js
@@ -1,53 +1,72 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+$('#two').on('click', function() {
+ 	var cityID = 'Boston,MA,USA';
+ 	processCityID(cityID);
+ 	});
 
+$('#three').on('click', function() {
+var cityID = 'Los Angeles,CA,USA';
+processCityID(cityID);
+});
 
-$(document).ready(function () {
-    
-    $(".buttons").on("click", function() {
-        var cityChoice = $(".list-group-item").val();
-        console.log(cityChoice);
+$('#four').on('click', function() {
+	var cityID = 'Seattle,WA,USA';
+	processCityID(cityID);
+	});
 
-        if (cityChoice === "boston") {
-            console.log("boston");
-            ajaxtranslate("https://elvish.p.rapidapi.com/sindarin.json?text=", cityChoice, "elvish.p.rapidapi.com");
-        }
-        else if (cityChoice === "los angeles") {
-            console.log("los angeles");
-            ajaxtranslate("https://valspeak.p.rapidapi.com/valspeak.json?text=", cityChoice, "valspeak.p.rapidapi.com");
-        }
-        else if (cityChoice === "seattle"){
-            console.log("seattle");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
-        
-        }
-        else if (cityChoice === "chicago"){
-            console.log("chicago");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
+$('#five').on('click', function() {
+	var cityID = 'Chicago,IL,USA';
+	processCityID(cityID);
+	});
+			
+$('#six').on('click', function() {
+	var cityID = 'Detroit,MI,USA';
+	processCityID(cityID);
+	});
 
-        }
-        else if (cityChoice === "detroit"){
-            console.log("detroit");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
-        
-        }
-        else if (cityChoice === "saint louis") {
-            console.log("saint louis");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
+$('#seven').on('click', function() {
+	var cityID = 'Saint Louis,USA';
+	processCityID(cityID);
+	});
 
-        }
-        else if (cityChoice === "miami"){
-            console.log("miami");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
+$('#eight').on('click', function() {
+	var cityID = 'Miami,FL,USA';
+	processCityID(cityID);
+	});
+	
+$('#nine').on('click', function() {
+	var cityID = 'Lincoln,NE,USA';
+	processCityID(cityID);
+	});
 
-        }
-        else if (cityChoice === "lincoln"){
-            console.log("lincoln");
-            ajaxtranslateSith("https://sith.p.rapidapi.com/sith.json?text=", cityChoice, "sith.p.rapidapi.com");
+	function processCityID(cityName){
+	//alert("test that city Id passed to a function as cityName " + cityName);
+	var fetchString = 'https://api.openweathermap.org/data/2.5/weather?q=';
+	var key = '&APPID=46aae4e318cb70fcc58425813d46824f';
+	var finalKey = fetchString + cityName + key
+	fetch(finalKey)
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		formatWeather(data); // Call formatWeather() to convert from Kelvin to degrees and display where needed
+		})
+}  //end processCityID function
 
-        }
+function formatWeather(d) {
+	//var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	// var fahrenheit = fahrenheit + '&deg;';
+	fahrenheit = fahrenheit + ' Degrees F';
+	var description = d.weather[0].description; 
+	var humidity = d.main.humidity;
+	var wind = d.wind.speed;
+	//wind = wind*2.23693629  ///converts wind from meters per second to MPH
+	wind = wind.toFixed(1);
+	var location = d.name;
+	location = location + ":  " + description;
 
-        });
-    });
-
-
+	$('h3').text(location);
+	$('#Temp').text("Temperature: " + fahrenheit);
+	$('#Humidity').text("Humidity: " + humidity);
+	$('#Wind').text("Wind speed: " + wind + " MPH");
+}
+	
